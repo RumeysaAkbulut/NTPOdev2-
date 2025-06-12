4ca239644ec49c2fc337951b64ced4b116eeb662
RumeysaAkbulut <168100590+RumeysaAkbulut@users.noreply.github.com>
2025-06-12 23:46:49 +0300

    Create KargoDurum.cs

---
+++ KargoDurum.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KargoTakip.Models
+{
+    public enum KargoDurum
+    {
+        Hazirlaniyor,
+        Yolda,
+        TeslimEdildi
+    }
+}
