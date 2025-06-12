9ea89bbaaa9fd3960e08880b8e42e7138446f34d
RumeysaAkbulut <168100590+RumeysaAkbulut@users.noreply.github.com>
2025-06-12 23:56:51 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+
+namespace KargoTakip
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+    }
