82c46d49ece10a6f7f7423e7f53845ab014b8b12
RumeysaAkbulut <168100590+RumeysaAkbulut@users.noreply.github.com>
2025-06-13 00:02:43 +0300

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,31 @@
+namespace WindowsFormsApp15
+{
+    partial class Form1 : System.Windows.Forms.Form
+    {
+  
+        private System.ComponentModel.IContainer components = null;
+
+       
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+             
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+
+        #region Windows Form Designer generated code
+
+        #endregion
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+             
+        }
+    }
+}
